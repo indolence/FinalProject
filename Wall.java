2faac53555b422c3b22ee20aac770a2f3f0d9021
djschneck906 <DJSchneck@student.naperville203.org>
2017-05-05 09:11:11 -0500

    inital outline

---
+++ Wall.java
@@ -0,0 +1,24 @@
+
+
+/**
+ * 
+ */
+public class Wall
+{
+    private int lx, rx, ty, by;
+    private Picture image;
+    
+    /**
+     * Default constructor for objects of class Wall
+     */
+    public Wall(int left, int right, int top, int bottom, Picture pic)
+    {
+        lx = left;
+        rx = right;
+        ty = top;
+        by = bottom;
+        pic = image;
+    }
+
+    
+}
